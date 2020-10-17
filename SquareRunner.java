f383eaed6c503c487c3cde3cdbe07625caa8182e
smilesMadhavi <72471453+smilesMadhavi@users.noreply.github.com>
2020-10-17 16:04:14 -0500

    Update SquareRunner.java

--- SquareRunner.java
+++ SquareRunner.java
@@ -1,12 +1,8 @@
 import static java.lang.System.*;
 public SquareRunner
-
 {
- 
-
   public static void main (String[] args)
-  
-   {
+     {
      Square test = new Square(5); 
      System.out.println (test.computePerimeter()); 
      System.out.println(test.computeArea()); 
@@ -18,3 +14,4 @@ public SquareRunner
 
 
    }
+}
